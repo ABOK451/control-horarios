3674079630aac840c963b161c020e280d0cc0a4e
Eduardo González Ortiz <eduardo4t8@gmail.com>
2024-03-13 15:28:13 -0600

    Primer Repositorio 13/03/2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { HomeComponent } from './components/home/home.component';
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 export const routes: Routes = [
   {
     path: 'home',
     component:HomeComponent
+  },
+  {
+    path: 'inicio',
+    component:InicioComponent
   },
   {
     path: '',
